a2891d693723d2f37256642d57e476335c10f6d2
IT22126092 <it22126092@my.sliit.lk>
2024-10-02 00:19:52 +0530

    Update idioms.js

--- src/components/Translator/idioms.js
+++ src/components/Translator/idioms.js
@@ -6,7 +6,15 @@ const idioms= {
     "he hit the sack":"ඔහු ගෝනියට පහර දුන්නේය",
     "i'm tired so i'm going to hit the sack":"මට මහන්සියි ඒ නිසා මම නිදාගන්නවා",
     "the exam was a piece of cake":"පරීක්ෂණය ඉතා පහසු ය",
-    "The math test was a piece of cake I finished it quickly":"ගණිත පරීක්ෂණය ඉතා පහසු විය; මට එය ඉක්මනින් නිම කළා.",
+    "the math test was a piece of cake i finished it quickly":"ගණිත පරීක්ෂණය ඉතා පහසු විය; මට එය ඉක්මනින් නිම කළා.",
+    "I didn't go to work today because i was feeling under the weather":"අද මම වැඩට ගියේ නැහැ අසනීපයක් නිසා",
+    "i wanted to apply for the job but i missed the boat":"මට රැකියාවට අයදුම් කරන්න හිතුනා, හැබැයි මම අවස්ථාව අහිමි කරගත්තා",
+    "he finished his homework late but better late than never":"ඔහු තම ගෙදර වැඩ ප්‍රමාද වී අවසන් කළ නමුත් එය කිසි විටෙකත් නොකර සිටීමට වඩා ප්‍රමාද වීම හොඳය",
+    "to make everyone feel comfortable she told a joke to break the ice":"සියල්ලන්ටම සුවපහසු හැඟීමක් ඇති කිරීමට, නිශ්ශබ්දතාව බිඳ දැමීමට ඇය විහිළුවක් කීවාය."
+
+
+
+
     
 
 }
